a0196a15fe784425d75d2eca603b66cde2f6439d
Felipe Antonio <96073664+InfoFelipe@users.noreply.github.com>
2023-03-18 15:12:25 -0300

    Initial commit

---
+++ src/br.edu.ifg.builder/br/edu/ifg/builder/builder.java
@@ -0,0 +1,9 @@
+
+public interface builder {
+
+    public void getLetras();
+    public void getCaracterEspecial();
+    public void gerarNumeros();
+    public void gerarLetrasMaiusculas();
+    
+}
---
+++ src/br.edu.ifg/client.java
@@ -0,0 +1,13 @@
+public class client {
+    String tipoSenha;
+    Number quantCaracter;
+
+    public void definirTipoSenha(){
+
+    }
+
+    public void definirQuantCaracter(){
+
+    }
+    
+}
