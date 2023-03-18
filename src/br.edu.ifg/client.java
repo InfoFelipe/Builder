d242709f3616d7cfdf7c7c35c929bcb71ebd52b1
Felipe Antonio <96073664+InfoFelipe@users.noreply.github.com>
2023-03-18 16:21:30 -0300

    Regra de negócio na classe Client
    Adicionado a classe Scanner do java para os atributos do tipo de senha e quantidade de caracter.

--- src/br.edu.ifg/client.java
+++ src/br.edu.ifg/client.java
@@ -1,13 +1,25 @@
+import java.util.Scanner;
+
 public class client {
     String tipoSenha;
     Number quantCaracter;
 
+    Scanner reader = new Scanner(System.in);
+
     public void definirTipoSenha(){
+
+        System.out.println("Digite o tipo de senha que deseja gerar: ");
+        tipoSenha = reader.nextLine();
 
     }
 
     public void definirQuantCaracter(){
 
+        System.out.println("Quantidade de caracter da senha: 6, 8, 16");
+        quantCaracter = reader.nextInt();
+
     }
+
     
+
 }
